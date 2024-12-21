32bf81c0bbf19a0c9aec5377364e38fa2b3971da
Naisokkhim <nai.sokkhim.1221@rupp.edu.kh>
2024-12-21 19:50:14 +0700

    nothing changed

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {Container} from 'react-bootstrap'
-import {GiHighFive} from 'react-icons/gi'
 export default function LandingPage() {
   return (
     <Container>
-        <h1 className='fw-bolder'>Hi, I'm Naisokkhim 👋<br />full-stack developer</h1>
+        <h1 className='fw-bolder'>Hi, I'm Naisokkhim👋<br />full-stack developer</h1>
+        
     </Container>
   )
 }
