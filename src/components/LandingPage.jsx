f584c98a1bce91f3c303658c8acefab1c7474a41
Naisokkhim <nai.sokkhim.1221@rupp.edu.kh>
2024-12-22 18:38:46 +0700

    add profile and sometext

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import {Container} from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import profileImage from'../assets/images/profile.png'
 export default function LandingPage() {
   return (
-    <Container>
-        <h1 className='fw-bolder'>Hi, I'm Naisokkhim👋<br />full-stack developer</h1>
-        
+    <Container className='d-flex flex-column justify-content-center align-items-center my-5'>
+      <Row className='w-100 my-5 d-flex ' >
+        <Col className='d-flex flex-column justify-content-center align-items-center text-start gap-1'>
+        <h1 className='fw-bolder'>Hello,I'm full-stack <br />Developer 👋</h1>
+        <p>I am passionate about coding,<br />experienced in web development,<br /> and always seeking opportunities to grow my skills.</p>
+
+        </Col>
+        <Col className='d-flex justify-content-center align-items-center'>
+          <img src={profileImage} alt="profile" />
+        </Col>
+      </Row>
+       
     </Container>
   )
 }
