14b7cef74c7b21b806dce96a3636abf2f3951eb3
Naisokkhim <nai.sokkhim.1221@rupp.edu.kh>
2024-12-24 15:46:30 +0700

    Add Some framwork icone

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,22 +1,62 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import profileImage from'../assets/images/profile.png'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import profileImage from "../assets/images/profile.png";
+import {
+  FaGithub,
+  FaFacebook,
+  FaTelegram,
+  FaReact,
+  FaJs,
+  FaCss3,
+  FaHtml5,
+} from "react-icons/fa";
+
 export default function LandingPage() {
   return (
-    <Container className='d-flex flex-column justify-content-center align-items-center my-5'>
-      <Row className='w-100 my-5 d-flex ' >
-        <Col className='d-flex flex-column justify-content-center align-items-center text-start gap-1'>
-        <h1 className='fw-bolder'>Hello,I'm full-stack <br />Developer 👋</h1>
-        <p>I am passionate about coding,<br />experienced in web development,<br /> and always seeking opportunities to grow my skills.</p>
+    <Container className="d-flex flex-column justify-content-center align-items-center my-5">
+      <Row
+        className=" my-5 d-flex justify-content-center align-items-center fRow"
+        style={{ gap: "200px" }}
+      >
+        <Col className=" d-flex flex-column justify-content-center align-items-start text-start gap-1">
+          <h1 className=" fw-bolder" style={{ width: "400px" }}>
+            Hello,I'm full-stack <br />
+            Developer 👋
+          </h1>
+          <p>
+            I am passionate about coding,
+            <br />
+            experienced in web development,
+            <br /> and always seeking opportunities to grow my skills.
+          </p>
 
+          <div style={{ display: "flex", gap: "20px" }}>
+            <FaGithub size={30} className="onHover" title="GitHub" />
+            <FaFacebook size={30} className="onHover" title="Facebook" />
+            <FaTelegram size={30} className="onHover" title="Telegram" />
+          </div>
         </Col>
-        <Col className='d-flex justify-content-center align-items-center'>
-          <img src={profileImage} alt="profile" />
+        <Col>
+          <img
+            src={profileImage}
+            alt="profile"
+            style={{
+              filter: "drop-shadow(0 0 0.2rem gray)",
+            }}
+          />
         </Col>
       </Row>
-       
+      <Row className="w-100 align-items-start" style={{ marginLeft: "24rem" }}>
+        <Col className="d-flex gap-3">
+          <p className="fw-bolder  ">Tech Skill |</p>
+          <FaReact color="#00FFFF" size={30} className="ms-2 moving onHover" />
+          <FaJs color="#FFD700" size={30} className="ms-2 onHover" />
+          <FaCss3 color="#4682B4" size={30} className="ms-2 onHover" />
+          <FaHtml5 color="#FF4500" size={30} className="ms-2 onHover" />
+        </Col>
+      </Row>
     </Container>
-  )
+  );
 }
