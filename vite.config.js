65385ea093b48ee02e9fcb9b57de08ef459e1df5
Naisokkhim <nai.sokkhim.1221@rupp.edu.kh>
2024-12-24 18:56:51 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Persional_Project/', // Ensure this matches the GitHub Pages URL path
   plugins: [react()],
+  base: '/Persional_Project', 
 })
